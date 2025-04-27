ddcade601e8c7cb149e97b71e2fb79914cdbd8fe
arisstogiannos <arsenis_sto@yahoo.gr>
2025-04-27 12:32:19 +0300

    colors

--- src/app/[locale]/(website)/get-started/[product]/layout.tsx
+++ src/app/[locale]/(website)/get-started/[product]/layout.tsx
@@ -1,13 +1,18 @@
 import React, { ReactNode } from "react";
 import Steps from "../_components/Steps";
+import Image from "next/image";
 
 
 export default async function layout({ children }: { children: ReactNode }) {
 
   return (
-    <div className="mt-0 space-y-14">
+    <div className="mt-0 space-y-10">
       <Steps />
-      <div className="border-4 border-accent rounded-3xl bg-background p-3 sm:p-8 w-fit min-h-[600px] mx-auto transition-all duration-1000 min-w-full lg:min-w-3xl shadow-lg">
+      <div className="border-4 border-accent rounded-3xl bg-background p-3  sm:p-8 w-fit min-h-[600px] mx-auto relative transition-all duration-1000 min-w-full lg:min-w-3xl shadow-lg">
+        <div className="absolute -left-0 -top-0  blur-3xl -z-10 size-80 bg-radial from-primary/50 to-background"  />
+        <div className="absolute right-0 top-30  blur-3xl -z-10 size-80 bg-radial from-purple-800/50 to-background"  />
+        <div className="absolute -left-10 bottom-30  blur-3xl -z-10 size-80 bg-radial from-purple-600/50 to-background"  />
+        <div className="absolute inset-x-0 bottom-0  blur-3xl -z-10 w-[120%] h-80 bg-radial from-cyan-500/50 to-background"  />
         {children}
       </div>
     </div>
--- src/app/[locale]/(website)/get-started/layout.tsx
+++ src/app/[locale]/(website)/get-started/layout.tsx
@@ -2,6 +2,6 @@ import React, { ReactNode } from 'react'
 
 export default function layout({children}:{children:ReactNode}) {
   return (
-    <div className='py-20 3xl:px-20'>{children}</div>
+    <div className='py-20 pt-10 3xl:px-20'>{children}</div>
   )
 }
