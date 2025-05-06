037703eda447a7b28af950b41bce9083a28ced22
arisstogiannos <arsenis_sto@yahoo.gr>
2025-05-06 19:36:44 +0300

    fix riminder route

--- src/app/api/send-reminders/route.ts
+++ src/app/api/send-reminders/route.ts
@@ -5,12 +5,12 @@ import { generateVerificationToken } from '@/lib/tokens';
 import { NotificationType } from '@prisma/client';
 
 export async function GET(request: NextRequest) {
-    //   const authHeader = request.headers.get('authorization');
-    //   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    //     return new Response('Unauthorized', {
-    //       status: 401,
-    //     });
-    //   }
+      const authHeader = request.headers.get('authorization');
+      if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return new Response('Unauthorized', {
+          status: 401,
+        });
+      }
 
     const users = await db.user.findMany({ select: { business: { select: { product: true, menu: { select: { published: true } } } }, email: true, name: true, id: true, emailVerified: true } })
 
