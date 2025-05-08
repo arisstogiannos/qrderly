ebf8f4b505cf26f65caf35faf0242cc0eace88f1
arisstogiannos <arsenis_sto@yahoo.gr>
2025-05-08 17:55:24 +0300

    s

--- src/app/[locale]/(business)/[businessName]/(menus)/menu/_templates/template1/MenuItems/MenuItemModal.tsx
+++ src/app/[locale]/(business)/[businessName]/(menus)/menu/_templates/template1/MenuItems/MenuItemModal.tsx
@@ -23,7 +23,7 @@ export default function MenuItemModal({
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerContent className="overflow-hidden rounded-t-3xl border-0 bg-secondary text-foreground">
         <div className="scrollbar-hidden max-h-[90vh]  overflow-y-auto pb-20 min-[350px]:max-h-[80vh] relative">
-          <div className="relative aspect-video h-auto w-full overflow-hidden rounded-lg">
+       { menuItem.imagePath &&<div className="relative aspect-video h-auto w-full overflow-hidden rounded-lg">
             <CloudImage
               src={menuItem.imagePath ?? ""}
               fill
@@ -33,7 +33,7 @@ export default function MenuItemModal({
             {/* <DrawerClose   className="absolute right-4 top-4 flex size-10 items-center justify-center rounded-full bg-background">
               <ChevronDown onClick={()=>setOpen(false)} size={"30px"} />
             </DrawerClose> */}
-          </div>
+          </div>}
 
           <div className=" px-4 pb-4 relative">{children}</div>
         </div>
@@ -46,14 +46,14 @@ export default function MenuItemModal({
     <DialogContent className="overflow-hidden  border-0 bg-background text-foreground focus:outline-none p-0"
 >
       <div className="scrollbar-hidden max-h-[90vh]  overflow-y-auto pb-20 min-[350px]:max-h-[80vh] relative">
-        <div className="relative aspect-video h-auto w-full overflow-hidden rounded-lg">
+       {menuItem.imagePath && <div className="relative aspect-video h-auto w-full overflow-hidden rounded-lg">
           <CloudImage
             src={menuItem.imagePath ?? ""}
             fill
             className="inset-0 w-screen object-cover"
             alt={menuItem.name}
           />
-        </div>
+        </div>}
 
         <div className=" px-4 pb-4 relative">{children}</div>
       </div>
