190eff498ee9ebc6e6ef30b74de3d9ad9e99a358
arisstogiannos <arsenis_sto@yahoo.gr>
2025-05-08 18:07:44 +0300

    header fix

--- src/app/[locale]/(business)/[businessName]/(menus)/menu/_templates/template1/MenuItems/MenuItemModalHeader.tsx
+++ src/app/[locale]/(business)/[businessName]/(menus)/menu/_templates/template1/MenuItems/MenuItemModalHeader.tsx
@@ -13,7 +13,7 @@ export default function MenuItemModalHeader({
   menuItem: MenuItemRequired;
 }) {
   return (
-    <DrawerHeader className=" sticky top-0 py-7 bg-gradient-to-b from-background to-transparent from-70% flex items-start justify-between text-foreground flex-row">
+    <DrawerHeader className=" sticky top-0 py-7 bg-gradient-to-b from-secondary to-transparent from-70% flex items-start justify-between text-foreground flex-row">
       <div className="space-y-4">
         <DrawerTitle className="font-normal ">{menuItem.name}</DrawerTitle>
         <DrawerDescription className="overflow-hidden">
