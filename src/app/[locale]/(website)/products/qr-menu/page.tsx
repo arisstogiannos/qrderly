b3e8ee001af3b99ebea5106572f649466ae0861e
arisstogiannos <arsenis_sto@yahoo.gr>
2025-05-16 18:03:02 +0300

    ftm main

--- src/app/[locale]/(website)/products/qr-menu/page.tsx
+++ src/app/[locale]/(website)/products/qr-menu/page.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ProductPage from "../_components/ProductPage";
 import { productsData } from "@/data";
-import {getTranslations, setRequestLocale} from 'next-intl/server';
+import { getTranslations, setRequestLocale } from "next-intl/server";
 import type { Metadata } from "next";
-import { GoogleTagManager } from "@next/third-parties/google";
 
 export const dynamic = "error";
 export async function generateMetadata(): Promise<Metadata> {
-  const t = await getTranslations('metadata');
+  const t = await getTranslations("metadata");
 
-  const meta = t.raw('qrMenu') as {
+  const meta = t.raw("qrMenu") as {
     title: string;
     description: string;
     keywords: Record<string, string>;
@@ -27,36 +26,33 @@ export async function generateMetadata(): Promise<Metadata> {
       description: meta.ogDescription,
       images: [
         {
-          url: '/og-image.png',
+          url: "/og-image.png",
           width: 1200,
           height: 630,
           alt: meta.ogAlt,
         },
       ],
-      locale: 'en_US',
-      type: 'website',
+      locale: "en_US",
+      type: "website",
     },
     twitter: {
-      card: 'summary_large_image',
-      site: '@yourTwitterHandle',
+      card: "summary_large_image",
+      site: "@yourTwitterHandle",
       title: meta.ogTitle,
       description: meta.ogDescription,
-      images: ['/og-image.png'],
+      images: ["/og-image.png"],
     },
   };
 }
 
-export default async function page({params}:{params: Promise<{locale: string}>}) {
+export default async function page({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
   const locale = (await params).locale;
-  
-  setRequestLocale(locale)
-  const smMenu = {...productsData[0]}
+  setRequestLocale(locale);
+  const smMenu = { ...productsData[0] };
   // smMenu.title = "QR Menu – Instant Access, Effortless Browsing"
-  return (
-    <>
-    <GoogleTagManager gtmId="GTM-W7D4ZMR5"  />
-    <ProductPage product={smMenu}/>
-    </>
-  );
+  return <ProductPage product={smMenu} />;
 }
-
