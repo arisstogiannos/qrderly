48a5782af225e81d500343916ed470f62c18ddb7
arisstogiannos <arsenis_sto@yahoo.gr>
2025-05-23 19:48:40 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -20,19 +20,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
+  
 
 };
 
